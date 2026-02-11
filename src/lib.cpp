a815ca0b2d9e43411651bfe82d5f204e0447968f
flpflan <flpflan@gmail.com>
2026-02-11 15:27:33 +0800

    feature: bypass XignCode3

--- src/lib.cpp
+++ src/lib.cpp
@@ -25,17 +25,22 @@ __attribute__((destructor)) void on_unload()
 }
 #else
 #include "Windows.h"
+
+static HMODULE g_hModule{};
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
 {
     switch (reason)
     {
     case DLL_PROCESS_ATTACH:
+        g_hModule = hModule;
+
         DisableThreadLibraryCalls(hModule);
 
         std::thread(webui::start).detach();
 
         umod::bootstrap::addFeature(freecam::kDesc);
-        std::thread(umod::bootstrap::run).detach();
+	std::thread(umod::bootstrap::run).detach();
         break;
 
     case DLL_PROCESS_DETACH:
@@ -51,5 +56,43 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
     }
 
     return TRUE;
+}
+
+#define X3_NOT_INITIALIZED -536805375
+
+typedef int32_t(__stdcall *t_x3_Dispatch)(OUT void *Function, IN uint32_t Type);
+static t_x3_Dispatch x3_Dispatch{};
+
+static std::wstring getCurrentDllDirectory()
+{ 
+    wchar_t path[MAX_PATH]{};
+    GetModuleFileNameW(g_hModule, path, MAX_PATH);
+    std::wstring sPath = {path};
+    const auto pos = sPath.find_last_of(L"\\/");
+    return sPath.substr(0, pos);
+}
+
+extern "C" __declspec(dllexport) int32_t x3_barrier_x64_1(void *FunctionAddress, uint32_t Type)
+{
+    if (x3_Dispatch == nullptr)
+    {
+        const auto orignalPath = getCurrentDllDirectory() + L"\\x3_x64.xem.bak";
+
+        HMODULE hX3 = LoadLibraryW(orignalPath.c_str());
+        if (hX3 == nullptr)
+        {
+            MessageBoxA(0, "LoadLibraryA failed!", "Error", 0);
+            return X3_NOT_INITIALIZED;
+        }
+
+        x3_Dispatch = reinterpret_cast<t_x3_Dispatch>(GetProcAddress(hX3, reinterpret_cast<LPCSTR>(1)));
+        if (x3_Dispatch == nullptr)
+        {
+            MessageBoxA(0, "GetProcAddress failed!", "Error", 0);
+            return X3_NOT_INITIALIZED;
+        }
+    }
+
+    return x3_Dispatch(FunctionAddress, Type);
 }
 #endif

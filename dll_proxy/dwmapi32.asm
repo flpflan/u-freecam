7f2c194ab19b02ed8b8f644d197a5a2bdf3b6ba4
flpflan <flpflan@gmail.com>
2026-05-28 20:05:41 +0800

    fix x86 build

---
+++ dll_proxy/dwmapi32.asm
@@ -0,0 +1,581 @@
+.386
+.model flat, stdcall
+option casemap:none
+
+.code
+
+extern mProcs:DWORD
+
+f0 proc
+    jmp DWORD PTR mProcs[4*0]
+f0 endp
+
+f1 proc
+    jmp DWORD PTR mProcs[4*1]
+f1 endp
+
+f2 proc
+    jmp DWORD PTR mProcs[4*2]
+f2 endp
+
+f3 proc
+    jmp DWORD PTR mProcs[4*3]
+f3 endp
+
+f4 proc
+    jmp DWORD PTR mProcs[4*4]
+f4 endp
+
+f5 proc
+    jmp DWORD PTR mProcs[4*5]
+f5 endp
+
+f6 proc
+    jmp DWORD PTR mProcs[4*6]
+f6 endp
+
+f7 proc
+    jmp DWORD PTR mProcs[4*7]
+f7 endp
+
+f8 proc
+    jmp DWORD PTR mProcs[4*8]
+f8 endp
+
+f9 proc
+    jmp DWORD PTR mProcs[4*9]
+f9 endp
+
+f10 proc
+    jmp DWORD PTR mProcs[4*10]
+f10 endp
+
+f11 proc
+    jmp DWORD PTR mProcs[4*11]
+f11 endp
+
+f12 proc
+    jmp DWORD PTR mProcs[4*12]
+f12 endp
+
+f13 proc
+    jmp DWORD PTR mProcs[4*13]
+f13 endp
+
+f14 proc
+    jmp DWORD PTR mProcs[4*14]
+f14 endp
+
+f15 proc
+    jmp DWORD PTR mProcs[4*15]
+f15 endp
+
+f16 proc
+    jmp DWORD PTR mProcs[4*16]
+f16 endp
+
+f17 proc
+    jmp DWORD PTR mProcs[4*17]
+f17 endp
+
+f18 proc
+    jmp DWORD PTR mProcs[4*18]
+f18 endp
+
+f19 proc
+    jmp DWORD PTR mProcs[4*19]
+f19 endp
+
+f20 proc
+    jmp DWORD PTR mProcs[4*20]
+f20 endp
+
+f21 proc
+    jmp DWORD PTR mProcs[4*21]
+f21 endp
+
+f22 proc
+    jmp DWORD PTR mProcs[4*22]
+f22 endp
+
+f23 proc
+    jmp DWORD PTR mProcs[4*23]
+f23 endp
+
+f24 proc
+    jmp DWORD PTR mProcs[4*24]
+f24 endp
+
+f25 proc
+    jmp DWORD PTR mProcs[4*25]
+f25 endp
+
+f26 proc
+    jmp DWORD PTR mProcs[4*26]
+f26 endp
+
+f27 proc
+    jmp DWORD PTR mProcs[4*27]
+f27 endp
+
+f28 proc
+    jmp DWORD PTR mProcs[4*28]
+f28 endp
+
+f29 proc
+    jmp DWORD PTR mProcs[4*29]
+f29 endp
+
+f30 proc
+    jmp DWORD PTR mProcs[4*30]
+f30 endp
+
+f31 proc
+    jmp DWORD PTR mProcs[4*31]
+f31 endp
+
+f32 proc
+    jmp DWORD PTR mProcs[4*32]
+f32 endp
+
+f33 proc
+    jmp DWORD PTR mProcs[4*33]
+f33 endp
+
+f34 proc
+    jmp DWORD PTR mProcs[4*34]
+f34 endp
+
+f35 proc
+    jmp DWORD PTR mProcs[4*35]
+f35 endp
+
+f36 proc
+    jmp DWORD PTR mProcs[4*36]
+f36 endp
+
+f37 proc
+    jmp DWORD PTR mProcs[4*37]
+f37 endp
+
+f38 proc
+    jmp DWORD PTR mProcs[4*38]
+f38 endp
+
+f39 proc
+    jmp DWORD PTR mProcs[4*39]
+f39 endp
+
+f40 proc
+    jmp DWORD PTR mProcs[4*40]
+f40 endp
+
+f41 proc
+    jmp DWORD PTR mProcs[4*41]
+f41 endp
+
+f42 proc
+    jmp DWORD PTR mProcs[4*42]
+f42 endp
+
+f43 proc
+    jmp DWORD PTR mProcs[4*43]
+f43 endp
+
+f44 proc
+    jmp DWORD PTR mProcs[4*44]
+f44 endp
+
+f45 proc
+    jmp DWORD PTR mProcs[4*45]
+f45 endp
+
+f46 proc
+    jmp DWORD PTR mProcs[4*46]
+f46 endp
+
+f47 proc
+    jmp DWORD PTR mProcs[4*47]
+f47 endp
+
+f48 proc
+    jmp DWORD PTR mProcs[4*48]
+f48 endp
+
+f49 proc
+    jmp DWORD PTR mProcs[4*49]
+f49 endp
+
+f50 proc
+    jmp DWORD PTR mProcs[4*50]
+f50 endp
+
+f51 proc
+    jmp DWORD PTR mProcs[4*51]
+f51 endp
+
+f52 proc
+    jmp DWORD PTR mProcs[4*52]
+f52 endp
+
+f53 proc
+    jmp DWORD PTR mProcs[4*53]
+f53 endp
+
+f54 proc
+    jmp DWORD PTR mProcs[4*54]
+f54 endp
+
+f55 proc
+    jmp DWORD PTR mProcs[4*55]
+f55 endp
+
+f56 proc
+    jmp DWORD PTR mProcs[4*56]
+f56 endp
+
+f57 proc
+    jmp DWORD PTR mProcs[4*57]
+f57 endp
+
+f58 proc
+    jmp DWORD PTR mProcs[4*58]
+f58 endp
+
+f59 proc
+    jmp DWORD PTR mProcs[4*59]
+f59 endp
+
+f60 proc
+    jmp DWORD PTR mProcs[4*60]
+f60 endp
+
+f61 proc
+    jmp DWORD PTR mProcs[4*61]
+f61 endp
+
+f62 proc
+    jmp DWORD PTR mProcs[4*62]
+f62 endp
+
+f63 proc
+    jmp DWORD PTR mProcs[4*63]
+f63 endp
+
+f64 proc
+    jmp DWORD PTR mProcs[4*64]
+f64 endp
+
+f65 proc
+    jmp DWORD PTR mProcs[4*65]
+f65 endp
+
+f66 proc
+    jmp DWORD PTR mProcs[4*66]
+f66 endp
+
+f67 proc
+    jmp DWORD PTR mProcs[4*67]
+f67 endp
+
+f68 proc
+    jmp DWORD PTR mProcs[4*68]
+f68 endp
+
+f69 proc
+    jmp DWORD PTR mProcs[4*69]
+f69 endp
+
+f70 proc
+    jmp DWORD PTR mProcs[4*70]
+f70 endp
+
+f71 proc
+    jmp DWORD PTR mProcs[4*71]
+f71 endp
+
+f72 proc
+    jmp DWORD PTR mProcs[4*72]
+f72 endp
+
+f73 proc
+    jmp DWORD PTR mProcs[4*73]
+f73 endp
+
+f74 proc
+    jmp DWORD PTR mProcs[4*74]
+f74 endp
+
+f75 proc
+    jmp DWORD PTR mProcs[4*75]
+f75 endp
+
+f76 proc
+    jmp DWORD PTR mProcs[4*76]
+f76 endp
+
+f77 proc
+    jmp DWORD PTR mProcs[4*77]
+f77 endp
+
+f78 proc
+    jmp DWORD PTR mProcs[4*78]
+f78 endp
+
+f79 proc
+    jmp DWORD PTR mProcs[4*79]
+f79 endp
+
+f80 proc
+    jmp DWORD PTR mProcs[4*80]
+f80 endp
+
+f81 proc
+    jmp DWORD PTR mProcs[4*81]
+f81 endp
+
+f82 proc
+    jmp DWORD PTR mProcs[4*82]
+f82 endp
+
+f83 proc
+    jmp DWORD PTR mProcs[4*83]
+f83 endp
+
+f84 proc
+    jmp DWORD PTR mProcs[4*84]
+f84 endp
+
+f85 proc
+    jmp DWORD PTR mProcs[4*85]
+f85 endp
+
+f86 proc
+    jmp DWORD PTR mProcs[4*86]
+f86 endp
+
+f87 proc
+    jmp DWORD PTR mProcs[4*87]
+f87 endp
+
+f88 proc
+    jmp DWORD PTR mProcs[4*88]
+f88 endp
+
+f89 proc
+    jmp DWORD PTR mProcs[4*89]
+f89 endp
+
+f90 proc
+    jmp DWORD PTR mProcs[4*90]
+f90 endp
+
+f91 proc
+    jmp DWORD PTR mProcs[4*91]
+f91 endp
+
+f92 proc
+    jmp DWORD PTR mProcs[4*92]
+f92 endp
+
+f93 proc
+    jmp DWORD PTR mProcs[4*93]
+f93 endp
+
+f94 proc
+    jmp DWORD PTR mProcs[4*94]
+f94 endp
+
+f95 proc
+    jmp DWORD PTR mProcs[4*95]
+f95 endp
+
+f96 proc
+    jmp DWORD PTR mProcs[4*96]
+f96 endp
+
+f97 proc
+    jmp DWORD PTR mProcs[4*97]
+f97 endp
+
+f98 proc
+    jmp DWORD PTR mProcs[4*98]
+f98 endp
+
+f99 proc
+    jmp DWORD PTR mProcs[4*99]
+f99 endp
+
+f100 proc
+    jmp DWORD PTR mProcs[4*100]
+f100 endp
+
+f101 proc
+    jmp DWORD PTR mProcs[4*101]
+f101 endp
+
+f102 proc
+    jmp DWORD PTR mProcs[4*102]
+f102 endp
+
+f103 proc
+    jmp DWORD PTR mProcs[4*103]
+f103 endp
+
+f104 proc
+    jmp DWORD PTR mProcs[4*104]
+f104 endp
+
+f105 proc
+    jmp DWORD PTR mProcs[4*105]
+f105 endp
+
+f106 proc
+    jmp DWORD PTR mProcs[4*106]
+f106 endp
+
+f107 proc
+    jmp DWORD PTR mProcs[4*107]
+f107 endp
+
+f108 proc
+    jmp DWORD PTR mProcs[4*108]
+f108 endp
+
+f109 proc
+    jmp DWORD PTR mProcs[4*109]
+f109 endp
+
+f110 proc
+    jmp DWORD PTR mProcs[4*110]
+f110 endp
+
+f111 proc
+    jmp DWORD PTR mProcs[4*111]
+f111 endp
+
+f112 proc
+    jmp DWORD PTR mProcs[4*112]
+f112 endp
+
+f113 proc
+    jmp DWORD PTR mProcs[4*113]
+f113 endp
+
+f114 proc
+    jmp DWORD PTR mProcs[4*114]
+f114 endp
+
+f115 proc
+    jmp DWORD PTR mProcs[4*115]
+f115 endp
+
+f116 proc
+    jmp DWORD PTR mProcs[4*116]
+f116 endp
+
+f117 proc
+    jmp DWORD PTR mProcs[4*117]
+f117 endp
+
+f118 proc
+    jmp DWORD PTR mProcs[4*118]
+f118 endp
+
+f119 proc
+    jmp DWORD PTR mProcs[4*119]
+f119 endp
+
+f120 proc
+    jmp DWORD PTR mProcs[4*120]
+f120 endp
+
+f121 proc
+    jmp DWORD PTR mProcs[4*121]
+f121 endp
+
+f122 proc
+    jmp DWORD PTR mProcs[4*122]
+f122 endp
+
+f123 proc
+    jmp DWORD PTR mProcs[4*123]
+f123 endp
+
+f124 proc
+    jmp DWORD PTR mProcs[4*124]
+f124 endp
+
+f125 proc
+    jmp DWORD PTR mProcs[4*125]
+f125 endp
+
+f126 proc
+    jmp DWORD PTR mProcs[4*126]
+f126 endp
+
+f127 proc
+    jmp DWORD PTR mProcs[4*127]
+f127 endp
+
+f128 proc
+    jmp DWORD PTR mProcs[4*128]
+f128 endp
+
+f129 proc
+    jmp DWORD PTR mProcs[4*129]
+f129 endp
+
+f130 proc
+    jmp DWORD PTR mProcs[4*130]
+f130 endp
+
+f131 proc
+    jmp DWORD PTR mProcs[4*131]
+f131 endp
+
+f132 proc
+    jmp DWORD PTR mProcs[4*132]
+f132 endp
+
+f133 proc
+    jmp DWORD PTR mProcs[4*133]
+f133 endp
+
+f134 proc
+    jmp DWORD PTR mProcs[4*134]
+f134 endp
+
+f135 proc
+    jmp DWORD PTR mProcs[4*135]
+f135 endp
+
+f136 proc
+    jmp DWORD PTR mProcs[4*136]
+f136 endp
+
+f137 proc
+    jmp DWORD PTR mProcs[4*137]
+f137 endp
+
+f138 proc
+    jmp DWORD PTR mProcs[4*138]
+f138 endp
+
+f139 proc
+    jmp DWORD PTR mProcs[4*139]
+f139 endp
+
+f140 proc
+    jmp DWORD PTR mProcs[4*140]
+f140 endp
+
+f141 proc
+    jmp DWORD PTR mProcs[4*141]
+f141 endp
+
+f142 proc
+    jmp DWORD PTR mProcs[4*142]
+f142 endp
+
+end
